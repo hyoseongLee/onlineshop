ccadfe0fbbde77f58cc12407320fedc68ffc9674
hyoseongLee <hyoseong123@naver.com>
2024-11-17 18:45:10 +0900

    2014.11.17

--- src/component/ProductCard.js
+++ src/component/ProductCard.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const ProductCard = () => {
+const ProductCard = ({item}) => {
   return (
     <div>
-        <img src='https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F7e%2F58%2F7e582b1ab76af61d94a8add5f898c62607050ffa.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]' />
-        <div> Conscious choice</div>
-        <div>title</div>
-        <div>price</div>
-        <div>신제품</div>
+        <img src={item?.img} />
+        <div> {item?.choice == true ? "Conscious choice" : ""}</div>
+        <div>{item?.title}</div>
+        <div>{item?.price}</div>
+        <div>{item?.new == true ? "신제품" : ""}</div>
     </div>
-  )
+  );
 };
 
 export default ProductCard;
